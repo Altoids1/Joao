9d57ca74335c0ac4f48a241da6f9fa1743e6b24b
Gary Lafortune <mom-puter@hotmail.com>
2021-04-21 13:53:13 -0500

    Moves Program class to its own .h and .cpp
    This will become relevant once I add a bizzillion more native functions and /table and so on.

---
+++ Program.cpp
@@ -0,0 +1,74 @@
+#include "Program.h"
+
+void Program::construct_natives()
+{
+	//TEXT MANIPULATION
+	definedFunctions["print"] = &NativeFunction("print", [](std::vector<Value> args)
+	{
+		if (args.size())
+		{
+			std::cout << args[0].to_string();
+			for (int i = 1; i < args.size(); ++i)
+			{
+				std::cout << '\t' << args[i].to_string();
+			}
+		}
+		std::cout << "\n";
+		return Value();
+	});
+	definedFunctions["tostring"] = &NativeFunction("tostring", [](std::vector<Value> args)
+	{
+		return Value(args[0].to_string());
+	});
+	definedFunctions["void_stellakafuhparenthessisluaunderscorestatewithacapitalscommaluaunderscoreallocfvoidstarud"] = &NativeFunction("void_stellakafuhparenthessisluaunderscorestatewithacapitalscommaluaunderscoreallocfvoidstarud", [](std::vector<Value> args)
+	{
+		return Value(7);
+	});
+
+	//MATHEMATICS
+	definedFunctions["sqrt"] = &NativeFunction("sqrt", [](std::vector<Value> args)
+	{
+		Value arg = args[0];
+		switch (arg.t_vType)
+		{
+		case(Value::vType::Double):
+			return Value(sqrt(arg.t_value.as_double));
+		case(Value::vType::Integer):
+			return Value(sqrt(arg.t_value.as_int));
+		case(Value::vType::Bool):
+			return Value(sqrt(arg.t_value.as_bool));
+		default:
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		}
+	});
+	definedFunctions["sin"] = &NativeFunction("sin", [](std::vector<Value> args)
+	{
+		Value arg = args[0];
+		switch (arg.t_vType)
+		{
+		case(Value::vType::Double):
+			return Value(sin(arg.t_value.as_double));
+		case(Value::vType::Integer):
+			return Value(sin(arg.t_value.as_int));
+		case(Value::vType::Bool):
+			return Value(sin(arg.t_value.as_bool));
+		default:
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		}
+	});
+	definedFunctions["cos"] = &NativeFunction("cos", [](std::vector<Value> args)
+	{
+		Value arg = args[0];
+		switch (arg.t_vType)
+		{
+		case(Value::vType::Double):
+			return Value(cos(arg.t_value.as_double));
+		case(Value::vType::Integer):
+			return Value(cos(arg.t_value.as_int));
+		case(Value::vType::Bool):
+			return Value(cos(arg.t_value.as_bool));
+		default:
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		}
+	});
+}
---
+++ Program.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Forward.h"
+#include "AST.h"
+
+class Program // this is pretty much what Parser is supposed to output, and what Interpreter is supposed to take as input.
+{
+	std::unordered_map<std::string, Function*> definedFunctions;
+	std::unordered_map<std::string, ObjectType*> definedObjTypes;
+	Interpreter* myinterp{ nullptr };
+public:
+	enum class ErrorCode : int {
+		BadArgType,
+		NotEnoughArgs
+	};
+	Program()
+	{
+		//Construct all the native functions
+		construct_natives();
+	}
+	Program(Function* f)
+	{
+		definedFunctions["main"] = f;
+		construct_natives();
+	}
+	void set_interp(Interpreter& interp)
+	{
+		myinterp = &interp;
+	}
+	void construct_natives();
+
+	// I wanna point out that this is distinct from Interpreter's version of this function; it's a raw call to a function's name, directory data and all, while Interpreter's resolves scope first.
+	Function* get_func(std::string name)
+	{
+		if (!definedFunctions.count(name))
+			return nullptr;
+		return definedFunctions[name];
+	}
+	void set_func(std::string name, Function* f)
+	{
+		definedFunctions[name] = f;
+	}
+
+};
