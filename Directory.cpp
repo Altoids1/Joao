f033059509dd833095c0b0cdeba5fac8f759a94d
Gary Lafortune <mom-puter@hotmail.com>
2021-04-23 15:23:07 -0500

    Shunts Directory:: implementation into its own .cpp
    LNK2005 is my mortal enemy

---
+++ Directory.cpp
@@ -0,0 +1,26 @@
+#include "Forward.h"
+#include "Directory.h"
+
+std::string Directory::DotDot(std::string dir) // Go up one directory from this directory
+{
+	size_t lastslash = dir.find_last_of('/');
+	if (lastslash == std::string::npos)
+		return "";
+	return dir.erase(lastslash, std::string::npos);
+}
+
+std::string Directory::lastword(std::string dir)
+{
+	size_t lastslashpos = dir.find_last_of('/');
+
+	if (lastslashpos == std::string::npos) // If this directory lacks a slash (???)
+	{
+		return dir; // return itself
+	}
+	else if (dir[lastslashpos] == dir.back()) // If the slash is at the very end
+	{
+		return "";
+	}
+
+	return dir.substr(lastslashpos + 1, std::string::npos);
+}
--- Directory.h
+++ Directory.h
@@ -3,14 +3,11 @@
 /*
 A namespace of helper functions for the manipulation of strings that indicate a directory.
 */
-
 namespace Directory
 {
-	std::string DotDot(std::string dir) // Go up one directory from this directory
-	{
-		size_t lastslash = dir.find_last_of('/');
-		if (lastslash == std::string::npos)
-			return "";
-		return dir.erase(lastslash, std::string::npos);
-	}
+	// Go up one directory from this directory
+	std::string DotDot(std::string);
+
+	// Get the last word token within this dir, returning emptystring if there is none and dir if there's no slash
+	std::string lastword(std::string);
 }
