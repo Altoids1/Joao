df2a89c9b21e66c892ec84c5ee2ded3ada456b23
Gary Lafortune <mom-puter@hotmail.com>
2021-05-08 01:52:01 -0500

    Implements a basic table library, incl. constructor
    Done remote so, hopefully it'll compile?

--- nativefuncs/tablelib.cpp
+++ nativefuncs/tablelib.cpp
@@ -1,13 +1,109 @@
 #include "../Program.h"
 #include "../Object.h"
 
-#define NATIVE_FUNC_TABLE(name) static_cast<Function*>(new NativeFunction( name , [](std::vector<Value> args)
-
-
 void Program::construct_table_library()
 {
 	definedObjTypes["/table"] = new ObjectType("/table");
 	definedObjTypes["/table"]->is_table_type = true;
-	//definedObjTypes["/table"]->set_typemethod_raw("insert",)
+	
+	definedObjTypes["/table"]->set_typemethod_raw("#constructor",new NativeMethod("#constructor",[](std::vector<Value> args, Object* obj){
+		
+		Table* t = static_cast<Table*>(obj);
 
+		for(size_t i = 0; i < args.size(); ++i)
+		{
+			t->t_array.push_back(args[i]);
+		}
+
+		return Value(obj); // If anything.
+	}));
+	definedObjTypes["/table"]->set_typemethod_raw("implode",new NativeMethod("implode",[](std::vector<Value> args, Object* obj){
+		Table* t = static_cast<Table*>(obj);
+
+		std::string sep = "";
+		size_t start = 0;
+		size_t stop = t->t_array.size() - 1;
+
+		switch(args.size())
+		{
+		default:
+		case(3):
+			if(args[2].t_vType != Value::vType::Integer)
+				return Value(Value::vType::Null, int(ErrorCode::BadArgtype));
+			stop = args[2];
+		case(2):
+			if(args[1].t_vType != Value::vType::Integer)
+				return Value(Value::vType::Null, int(ErrorCode::BadArgtype));
+			start = args[1];
+		case(1):
+			sep = args[0].to_string();
+			break;
+		case(0):
+			return Value(Value::vType::Null, int(ErrorCode::NotEnoughArgs));
+		}
+
+		if(args.size() < 1)
+			sep = args[0].to_string();
+		
+		std::string result = "";
+
+		for(start; start <= stop && start < t->t_array.size(); ++start)
+		{
+			result += t->t_array[start];
+		}
+		return Value(result);
+	}));
+
+
+	definedObjTypes["/table"]->set_typemethod_raw("insert",new NativeMethod("insert",[](std::vector<Value> args, Object* obj){
+		if(args.size() < 2)
+			return Value(Value::vType::Null, int(ErrorCode::NotEnoughArgs));
+		if(args[0].t_vType != Value::vType::Integer)
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		
+		size_t index = args[0].t_value.as_int;
+
+		//This tries to mimic some of the behavior of at_set() w/o explicitly calling it
+		//FIXME: This should really all use the same function tbh
+
+		Table* t = static_cast<Table*>(obj);
+
+		if(index > t->t_array.size())
+		{
+			t->t_array.resize(index,Value());
+			t->t_array.push_back(args[1]);
+		}
+		else if(index ==  t->t_array.size())
+		{
+			t->t_array.push_back(args[1]);
+		}
+		else
+		{
+			t->t_array.insert(index,args[1]);
+		}
+		return Value();
+	}));
+	definedObjTypes["/table"]->set_typemethod_raw("remove",new NativeMethod("remove",[](std::vector<Value> args, Object* obj){
+		if(args.size() < 1)
+			return Value(Value::vType::Null, int(ErrorCode::NotEnoughArgs));
+		if(args[0].t_vType != Value::vType::Integer)
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		
+		size_t index = args[0].t_value.as_int;
+
+		//This tries to mimic some of the behavior of at_set() w/o explicitly calling it
+		//FIXME: This should really all use the same function tbh
+
+		Table* t = static_cast<Table*>(obj);
+
+		if(index >= t->t_array.size())
+		{
+			return Value();
+		}
+
+		t->t_array.erase(index,args[1]);
+		return Value();
+	}));
 }
+
+#undef NATIVE_FUNC_TABLE
