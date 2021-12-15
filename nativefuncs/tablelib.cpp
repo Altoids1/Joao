4c4fb665e90ec766d253da0ba6546597800191a4
Gary Lafortune <mom-puter@hotmail.com>
2021-12-15 09:36:14 -0600

    Reimplements /table.remove and /table.insert

--- nativefuncs/tablelib.cpp
+++ nativefuncs/tablelib.cpp
@@ -72,26 +72,69 @@ ObjectType* Program::construct_table_library()
 			return Value(args[rand() % args.size()]);
 		});
 
+	table->set_typemethod_raw("insert", new NativeMethod("remove", [](std::vector<Value> args, Object* obj) {
+		if (args.size() < 2)
+			return Value(Value::vType::Null, int(ErrorCode::NotEnoughArgs));
+		const Value& vindex = args[0];
+		Value::JoaoInt index = 0;
+		Table* table = static_cast<Table*>(obj);
+		switch (vindex.t_vType)
+		{
+		default:
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		case(Value::vType::Double):
+			index = math::round({ vindex.t_value.as_double }).t_value.as_int;
+			[[fallthrough]];
+		case(Value::vType::Integer):
+			index = index || vindex.t_value.as_int; // Best I could come up with
+			//Since we're in /table/insert(), and not Table::at_set() or whatever,
+			//We have to make a point of, like, actually inserting into t_array if necessary.
+			if (index >= 0)
+			{
+				auto& arr = table->t_array;
+				if (index < arr.size()) // if we're trying to insert at a place where an element already exists
+				{
+					auto& hash = table->t_hash;
+					//Before we do this, imagine that there's an index [9] present in the hashtable, and the array only goes from [0] to [8].
+					//Inserting naïvely in that case would clobber the 9th element, which is a bad.
+
+					//talloc() tries to prevent this, but it's still possible, so lets do some checks first.
+					while (hash.count(std::hash<Value::JoaoInt>()(arr.size()))) // If this is the case
+					{ // take that element and actually put it on the fucking array, I guess
+						Value::JoaoInt hash_index = std::hash<Value::JoaoInt>()(arr.size());
+						arr.push_back(std::move(hash.at(hash_index)));
+						hash.erase(hash_index);
+					}
+
+					arr.insert(arr.begin() + index, args[1]);
+					return Value(); // Skips a lot of the silliness of talloc(), which is nice, even if insert() is expensive.
+				}
+			}
+			//Otherwise, fallthrough to just using talloc() like normal.
+			[[fallthrough]];
+		case(Value::vType::String):
+			if (table->at_set_raw(vindex, args[1]))
+			{
+				return Value(Value::vType::Null, int(ErrorCode::FailedOperation)); // I'unno.
+			}
+			return Value();
+		}
+	}));
+
 	table->set_typemethod_raw("remove",new NativeMethod("remove",[](std::vector<Value> args, Object* obj){
 		if(args.size() < 1)
 			return Value(Value::vType::Null, int(ErrorCode::NotEnoughArgs));
-		if(args[0].t_vType != Value::vType::Integer)
-			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
-		
-		size_t index = args[0].t_value.as_int;
-
-		//This tries to mimic some of the behavior of at_set() w/o explicitly calling it
-		//FIXME: This should really all use the same function tbh
-
-		Table* t = static_cast<Table*>(obj);
-
-		if(index >= t->t_array.size())
+		const Value& vindex = args[0];
+		switch (vindex.t_vType)
 		{
+		default:
+			return Value(Value::vType::Null, int(ErrorCode::BadArgType));
+		case(Value::vType::Integer):
+		case(Value::vType::String):
+		case(Value::vType::Double):
+			static_cast<Table*>(obj)->tfree(vindex);
 			return Value();
 		}
-
-		t->t_array.erase(t->t_array.begin() + index);
-		return Value();
 	}));
 
 	return table;
