f512223ddc21cdbd3f35d846c39dcfe2996b37b7
Gary Lafortune <mom-puter@hotmail.com>
2022-01-24 16:48:17 -0600

    Adds some novel documentation
    Been trying to write these docs for a while but gave up on it in part because I didn't really like how the HTML generators were handling things.
    
    I've finally decided to say screw it and write it all manually in HTML-CSS-JS. So far, I'm pretty satisfied with the results.

---
+++ docs/joaocode_formatter.js
@@ -0,0 +1,56 @@
+function joao_format(str)
+{
+    //KEYWORDS
+    let keyword_color = '#f00';
+    str = str.replace(/^\/main/gm,"/<font color='" + keyword_color + "'>main</font>"); // Snowflake thing for main since it needs the slash
+    let keywords = ["if", "elseif","else","for","while", "throw","try","catch"];
+    for(index in keywords)
+    {
+        let key = keywords[index];
+        let reg = RegExp("\\b" + key + "\\b","gm");
+        str = str.replace(reg,"<font color='" + keyword_color + "'>" + key + "</font>");
+    }
+
+    //TYPES
+    let type_color = '#e88';
+    let types = [ "Number", "Object", "String","Value","table","file"];
+    for(index in types)
+    {
+        let type = types[index];
+        let reg = RegExp("\\b" + type + "\\b","gm");
+        str = str.replace(reg,"<font color='" + type_color + "'>" + type + "</font>");
+    }
+
+    //FUNCTIONS
+    let function_color = '#42c2ff';
+    let functions = ['print','New'];
+    for(index in functions)
+    {
+        let func = functions[index];
+        let reg = RegExp("\\b" + func + "\\b","gm");
+        str = str.replace(reg,"<font color='" + function_color + "'>" + func + "</font>");
+    }
+
+    //STRINGS
+    let str_color = '#f55';
+    str = str.replace(/"(.*?)"/gm,"<font color='" + str_color + "'>\"$1\"</font>");
+
+    //COMMENTS
+    let comment_color = '#88da88';
+    str = str.replace(/(\#\#.*)$/gm, "<font color='" + comment_color +"'>$1</font>")
+
+    return str;
+};
+
+let codes = document.getElementsByClassName("joaocode");
+
+for(index in codes)
+{
+    let element = codes[index];
+    console.log(element);
+    console.log(element.innerHTML);
+    if(element.innerHTML)
+    {
+        element.innerHTML = joao_format(element.innerHTML);
+    }
+}
