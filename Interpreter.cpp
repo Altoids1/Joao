6a17fc9ad30c79c79ae2f587bb7a612998adb604
Gary Lafortune <mom-puter@hotmail.com>
2022-03-14 19:50:47 -0500

    ObjectType::makeobject() now takes rvalue args, fixing bug
    I've had it up to here with const-ref binding shenanigans. It's all rvalues now. Screw it.
    
    I understand the C++ spec on rvalue lifespans just enough to believe that this doesn't cause dangling rvalue refs. All test passed so...

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -308,15 +308,6 @@ Value Interpreter::makeObject(std::string str, std::vector<ASTNode*>& args, ASTN
 	return Value(prog->definedObjTypes[str]->makeObject(*this, std::move(eval_args)));
 }
 
-Value Interpreter::makeObject(std::string str, const std::vector<Value>& eval_args, ASTNode* maker)
-{
-
-	if (!(prog->definedObjTypes.count(str)))
-		RuntimeError(maker, "Constructor attempts to instantiate unknown type! (" + str + ")"); // FIXME: This should really be a Parsetime error
-
-	return Value(prog->definedObjTypes[str]->makeObject(*this, eval_args));
-}
-
 Value Interpreter::makeBaseTable()
 {
 	return Value(prog->definedObjTypes["/table"]->makeObject(*this, {}));
